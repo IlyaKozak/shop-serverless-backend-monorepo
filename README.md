7b54e70a92e9c7a7e20f80c080fbe32e2f0934d8
Ilya Kozak <kozak@tut.by>
2021-09-13 13:11:17 +0300

    docs: correct readme for import service

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 - Run `npm i` to install the project dependencies
 - Run `npm run deploy` to deploy this stack to AWS
 - Run `npm test` to test lambda functions
+
+## Import Service
+- Go to Import Service folder:  `cd import-service`
+- See `README.md` in the Import Service folder
+- Change `.env.example` to `.env` and fill out with correct environment variables
+- Run `npm i` to install the project dependencies
+- Run `npm run deploy` to deploy this stack to AWS
+- Run `npm test` to test lambda functions
--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -16,33 +16,46 @@ const Bucket = process.env.S3_BUCKET;
 const s3 = new AWS.S3({ region: AWS_REGION });
 
 const importFileParser = async (event: S3Event) => {
-  console.log('FILE PARSER S3 ObjectCreated EVENT: ', event);
+  console.log('EVENT: ', event);
+  
+  for (const record of event.Records) {
+    const { key } = record.s3.object;
 
-  event.Records.forEach((record) => {
-    s3.getObject({
-      Bucket,
-      Key: record.s3.object.key,
-    })
-    .createReadStream()
-    .pipe(csv())
-    .on('data', (data) => {
-      console.log(data);
-    })
-    .on('end', async () => {
-      await s3.copyObject({
+    await new Promise((resolve, reject) => {
+      const s3Stream = s3.getObject({
         Bucket,
-        CopySource: `${Bucket}/${record.s3.object.key}`,
-        Key: record.s3.object.key.replace(S3_BUCKET_FOLDER_UPLOAD, S3_BUCKET_FOLDER_PARSE),
-      }).promise();
+        Key: key,
+      }).createReadStream();
+  
+      s3Stream.pipe(csv())
+        .on('data', (data) => {
+          console.log(data);
+        })
+        .on('error', (error) => {
+          reject(error);
+        })
+        .on('end', async () => {
+          const newKey = record.s3.object.key.replace(S3_BUCKET_FOLDER_UPLOAD, S3_BUCKET_FOLDER_PARSE);
+  
+          await s3.copyObject({
+            Bucket,
+            CopySource: `${Bucket}/${record.s3.object.key}`,
+            Key: newKey,
+          }).promise();
+        
+          await s3.deleteObject({
+            Bucket,
+            Key: key,
+          }).promise();
 
-      console.log(`
-        COPIED FROM ${Bucket}/${record.s3.object.key} TO ${Bucket}/${record.s3.object.key}
-      `);
-    })
-    .on('error', (error) => {
-      console.error('[ERROR] FILE PARSER S3 ObjectCreated ERROR: ', error);
-    })
-  });
-}
+          console.log(`
+            COPIED FROM ${Bucket}/${key} TO ${Bucket}/${newKey}
+          `);
+
+          resolve('done');
+        });
+    });
+  }
+};
 
 export const main = middyfy(importFileParser);
