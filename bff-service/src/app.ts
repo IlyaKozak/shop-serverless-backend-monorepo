f1a65de9b5219b23175e4c9cf879027de7988482
Ilya Kozak <kozak@tut.by>
2021-10-26 17:16:27 +0300

    feat: add bff service express app

--- bff-service/src/app.ts
+++ bff-service/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import axios, { AxiosRequestConfig, Method } from 'axios';
 
 const app = express();
 
@@ -10,6 +11,48 @@ app.use('/', (req: Request, res: Response, next: NextFunction) => {
     return;
   }
   next();
+});
+
+
+app.all('/*', async (req: Request, res: Response) => {
+  const { originalUrl, method, body } = req;
+  console.log(`originalUrl: ${originalUrl}, method: ${method}, body: ${JSON.stringify(body)}`);
+
+  const recipient = originalUrl.split('/')[1];
+  const recipientUrl = process.env[recipient];
+
+  if (recipientUrl) {
+    const axiosConfig: AxiosRequestConfig = {
+      method: method as Method,
+      url: `${recipientUrl.replace(/\/*$/, '')}${req.originalUrl.replace(/\/[^/]*/, '')}`,
+      ...(Object.keys(body || {}).length > 0 && { data: body })
+    };
+
+    console.log(`axiosConfig: ${JSON.stringify(axiosConfig)}`);
+
+    try {
+      const response = await axios(axiosConfig);
+      res.json(response.data);
+    } catch (error) {
+      if (error.response) {
+        const {
+          status,
+          data
+        } = error.response;
+
+        res.status(status).json(data);
+        return;
+      }
+
+      res.status(500).json({
+        error: error.message,
+      });
+    }
+  } else {
+    res.status(502).json({
+      error: 'Cannot process request',
+    });
+  }
 });
 
 export default app;
---
+++ bff-service/src/common/constants.ts
@@ -0,0 +1,4 @@
+export enum SERVICE_URL {
+  cart = 'CART_SERVICE_URL',
+  product = 'PRODUCT_SERVICE_URL',
+};
