c3b428fabc1c23f56541b03c05a7d74455a5b650
Ilya Kozak <kozak@tut.by>
2021-10-28 15:26:59 +0300

    feat: implement main functionality for bff service using nestjs

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { bffMiddleware } from './middlewares/bff.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  app.use(bffMiddleware);
+
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
